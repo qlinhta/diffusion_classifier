1efddc13effbb9e6ed4c66d2a123c17759ea339b
Quyen Linh TA <60761870+qlinhta@users.noreply.github.com>
2024-07-27 16:16:27 +0200

    Update :: --push --id: diffusion classifier

--- setup.sh
+++ setup.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-python3 -m venv env
+python3.9 -m venv env
+
 source env/bin/activate
 
-pip install -r requirements.txt
+pip install ipdb flake8 ipykernel pytest pytest-env tqdm numpy torch torchvision wandb tensorboard transformers diffusers accelerate seaborn
+pip install git+https://github.com/modestyachts/ImageNetV2_pytorch
+pip install datasets
+pip install git+https://github.com/openai/CLIP.git
+pip install open_clip_torch
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    pip install torch torchvision cudatoolkit
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/mps
+fi
 
 export CACHE_NUMBER=0
 
-echo "Installation complete and virtual environment activated."
+echo "Setup completed successfully."
